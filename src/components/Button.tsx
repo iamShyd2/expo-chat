87bf91c4344407a092fed5fa130e6a152aac28e5
Kwame Shyd <knana6523@gmail.com>
2022-11-10 09:37:44 +0000

    feat: style button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { COLORS } from "src/theme";
 import Text from "./Text";
 
 interface IButtonProps extends TouchableOpacityProps {
@@ -11,15 +13,20 @@ const Button: FC<IButtonProps> = ({ children, onPress, isLoading, }) => {
     return (
         <TouchableOpacity
             onPress={onPress}
-            style={[styles.button]}
+            style={[styles.button, { marginTop: RFPercentage(2) }]}
             disabled={isLoading}
         >
             {
                 isLoading ?
-                    <ActivityIndicator />
+                    <ActivityIndicator 
+                        style={[styles.indicator]}
+                        color={COLORS.seconday}
+                    />
                 : null
             }
-            <Text>
+            <Text
+                style={[styles.text]}
+            >
                 {children}
             </Text>
         </TouchableOpacity>
@@ -29,6 +36,17 @@ const Button: FC<IButtonProps> = ({ children, onPress, isLoading, }) => {
 const styles = StyleSheet.create({
     button: {
         flexDirection: "row",
+        justifyContent: "center",
+        backgroundColor: COLORS.primary,
+        paddingVertical: RFPercentage(2),
+        borderRadius: 50,
+    },
+    indicator: {
+        marginRight: RFPercentage(2),
+    },
+    text: {
+        fontFamily: "loraBold",
+        color: "#fff",
     }
 })
 
